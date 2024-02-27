7efb4fc0167a32f93bd52e7f55bc24ec85aa152c
Harendra Ranjan <Ranjanharendr@gmail.com>
2024-02-27 21:52:59 +0530

    add mul file

---
+++ mul.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class mul {
+     public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("Sum of a and b"+a*b);
+    }
+}
